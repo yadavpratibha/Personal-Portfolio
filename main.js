451c9a63e1f343587a83e37cf44701763d9d8c60
Pratibha Yadav <pratibha.yadav97@gmail.com>
2026-03-07 13:59:03 +0530

    home section completed

--- main.js
+++ main.js
@@ -10,36 +10,45 @@ if (navToggle) {
 
 //MENU HIDDEN
 if (navClose) {
-    navClose.addEventListener('click', () =>{
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink=document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction=()=>{
-    const navMenu=document.getElementById('nav-menu')
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu') //When we click each nav__link, we remove the show-menu class
 }
-navLink.forEach(n=>n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //CHANGE BACKGROUND HEADER
-const scrollHeader=()=>{
-    const header=document.getElementById('header')
-    this.scrollY >= 50 ?header.classList.add('scroll-header'):header.classList.remove('scroll-header')
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
 }
 
 
 /*=============== HOME TYPED JS ===============*/
 const typedHome = new Typed('#home-typed', {
-    strings: ['Software Engineer','Web developer','Computer Science Student'], //professions
-    typeSpeed: 50,
+    strings: ['Software Engineer', 'Web developer', 'Computer Science Student'], //adding professions
+    typeSpeed: 80,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: '_',
 });
 
 /*=============== ADD SHADOW HEADER ===============*/
-
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    //Add a class if bottom offset is >50 of viewport
+    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader);
 
 /*=============== CONTACT EMAIL JS ===============*/
 
